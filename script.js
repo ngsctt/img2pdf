2cdd22ea3bfa5f214d30aab5668abbb45c0417db
Glitch (glitch-hello-website) <none>
2023-03-25 07:07:58 +0000

    👡🏥 Checkpoint ./script.js:56317972/288 ./index.html:56317972/11

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const upload = document.getElementById('upload');
+const list = document.getElementById('list');
 const go = document.getElementById('go');
 const ppmm = document.getElementById('ppmm');
 
 const PPMM = 5.91;  // (equivalent to 150dpi)
 
+function checkFormat (mimetype) {
+  return true;
+}
+
 function addImage (files) {
-  if (!window.i)
+  if (!window.imageList) window.imageList = [];
+  
+  for (const file of files) {
+    if (checkFormat(file.type)) window.imageList.push(file);
+  }
+}
+
+function listImages () {
+  
 }
 
 function generate () {}
